900bb65d284d2c30dddf52e60235af328a78adb1
ningzixin <1051099173@qq.com>
2018-07-25 22:27:50 +0800

    53 dp

--- 53Maximum Subarray.py
+++ 53Maximum Subarray.py
@@ -4,3 +4,16 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        ll = [] # 状态 记录以当前字符结尾的子串的最大数
+        ll = nums
+        i = 1
+        maxNum = nums[0]
+        while i < len(nums):
+            if(ll[i-1]<=0):
+                ll[i] = nums[i]
+            else:
+                ll[i] = nums[i-1] + nums[i]
+            maxNum = max(ll[i],maxNum)
+            i += 1
+        return maxNum
+print(Solution.maxSubArray("",[-2,1,-3,4,-1,2,1,-5,4]))
